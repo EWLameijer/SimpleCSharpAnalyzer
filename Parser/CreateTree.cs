90ff1df006adc28f7e047f1a9af1c38a02fe5e7c
EWLameijer <eric_wubbo@hotmail.com>
2022-10-22 13:59:51 +0200

    Working on UsingDirectiveNode

--- Parser/CreateTree.cs
+++ Parser/CreateTree.cs
@@ -1,15 +1,31 @@
 ﻿using Tokenizing;
+using static Tokenizing.TokenType;
 
 namespace Parser;
 
+public class ParsePosition
+{
+    public int CurrentIndex { get; set; }
+    public IReadOnlyList<Token> Tokens { get; set; }
+
+    public Token CurrentToken() => Tokens[CurrentIndex];
+
+    public TokenType CurrentTokenType() => Tokens[CurrentIndex].TokenType;
+
+    public void Proceed() => CurrentIndex++;
+}
+
 public class CreateTree
 {
-    private readonly IReadOnlyList<Token> _tokens;
-    private readonly int _currentIndex = 0;
+    private readonly ParsePosition _position;
+
+    private TokenType CurrentTokenType() => _position.Tokens[_position.CurrentIndex].TokenType;
+
+    private void Proceed() => _position.CurrentIndex++;
 
     public CreateTree(IReadOnlyList<Token> tokens)
     {
-        _tokens = tokens;
+        _position = new ParsePosition { CurrentIndex = 0, Tokens = tokens };
     }
 
     public FileNode Parse()
@@ -26,27 +42,61 @@ public class CreateTree
 
     private UsingsNode GetUsingDirectives()
     {
-        throw new NotImplementedException();
+        // expect an using, or a namespace, or something else
+        List<UsingDirectiveNode> usingDirectives = new();
+        do
+        {
+            SkipWhitespace();
+            if (CurrentTokenType() == Using) usingDirectives.Add(UsingDirectiveNode.Get(_position));
+            else if (CurrentTokenType() == Namespace) { }
+            else { }
+        } while (true);
     }
-}
 
-public class Node
-{ }
-
-public class UsingsNode : Node
-{ }
-
-public class NamespaceNode : Node
-{ }
-
-public class FileNode : Node
-{
-    private readonly UsingsNode? _usingsNode;
-    private readonly NamespaceNode? _namespaceNode;
-
-    public FileNode(UsingsNode usingsNode, NamespaceNode namespaceNode)
+    private void SkipWhitespace()
     {
-        _usingsNode = usingsNode;
-        _namespaceNode = namespaceNode;
+        while (CurrentTokenType().IsSkippable()) Proceed();
+    }
+
+    public class Node
+    { }
+
+    public class UsingsNode : Node
+    { }
+
+    public class UsingDirectiveNode : Node
+    {
+        private readonly IReadOnlyList<Token> _contents;
+
+        public UsingDirectiveNode(List<Token> contents)
+        {
+            _contents = contents;
+        }
+
+        public static UsingDirectiveNode Get(ParsePosition position)
+        {
+            List<Token> contents = new();
+            while (position.CurrentTokenType() != SemiColon)
+            {
+                contents.Add(position.CurrentToken());
+                position.Proceed();
+            }
+            return new UsingDirectiveNode(contents);
+        }
+    }
+
+    public class NamespaceNode : Node
+    { }
+
+    public class FileNode : Node
+    {
+        private readonly UsingsNode? _usingsNode;
+        private readonly NamespaceNode? _namespaceNode;
+
+        public FileNode(UsingsNode usingsNode, NamespaceNode namespaceNode)
+        {
+            _usingsNode = usingsNode;
+            _namespaceNode = namespaceNode;
+        }
     }
 }
