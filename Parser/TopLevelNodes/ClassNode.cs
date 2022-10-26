b2e8ef880290e847a2b658b63816e22f38e0ca01
EWLameijer <eric_wubbo@hotmail.com>
2022-10-26 18:38:40 +0200

    retrieve parent class and implemented interfaces of a class

--- Parser/TopLevelNodes/ClassNode.cs
+++ Parser/TopLevelNodes/ClassNode.cs
@@ -8,5 +8,25 @@ internal class ClassNode : TopLevelNode
     public static ClassNode Get(ParsePosition position, List<Token> modifiers)
     {
         position.Proceed(); // skip "class" token
+        string className = ((ComplexToken)position.CurrentToken()).Info;
+        position.Proceed(); // expect : or {
+        List<Token> ancestors = GetAncestors(position);
+    }
+
+    private static List<Token> GetAncestors(ParsePosition position)
+    {
+        List<Token> implemented = new();
+        if (position.CurrentTokenType() == TokenType.Colon)
+        {
+            do
+            {
+                position.Proceed();
+                TokenType currentTokenType = position.CurrentTokenType();
+                if (currentTokenType == TokenType.Identifier) implemented.Add(position.CurrentToken());
+                else if (currentTokenType == TokenType.BracesOpen) break;
+                // skip whitespaces and commas
+            } while (true);
+        }
+        return implemented;
     }
 }
