4b8d68c231f271ed2073738a3c80624a4a497b94
EWLameijer <eric_wubbo@hotmail.com>
2022-10-26 08:43:55 +0200

    starting to work on class parsing

--- Parser/TopLevelNodes/ClassNode.cs
+++ Parser/TopLevelNodes/ClassNode.cs
@@ -1,5 +1,12 @@
-﻿namespace Parser.TopLevelNodes;
+﻿using Tokenizing;
+
+namespace Parser.TopLevelNodes;
 
 internal class ClassNode : TopLevelNode
 {
+    // first token is "class"
+    public static ClassNode Get(ParsePosition position, List<Token> modifiers)
+    {
+        position.Proceed(); // skip "class" token
+    }
 }
--- Parser/TopLevelNodes/TopLevelNode.cs
+++ Parser/TopLevelNodes/TopLevelNode.cs
@@ -1,5 +1,22 @@
-﻿namespace Parser.TopLevelNodes;
+﻿using Tokenizing;
+
+namespace Parser.TopLevelNodes;
 
 internal class TopLevelNode : Node
 {
+    public static TopLevelNode Get(ParsePosition position)
+    {
+        position.SkipWhitespace();
+        List<Token> modifiers = new();
+        while (position.CurrentTokenType().IsModifier())
+        {
+            modifiers.Add(position.CurrentToken());
+            position.Proceed();
+        }
+        return position.CurrentTokenType() switch
+        {
+            TokenType.Class => ClassNode.Get(position),
+            _ => throw new ArgumentException("TopLevelNode.Get: illegal data structure to parse!")
+        };
+    }
 }
